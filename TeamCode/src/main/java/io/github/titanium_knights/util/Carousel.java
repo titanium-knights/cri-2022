1a9f11d8da6ccc2d6f01579cfa7c29d612e91b21
alisona le <alile24@bergen.org>
2022-07-04 00:09:43 -0400

    carousel now works on both sides

--- TeamCode/src/main/java/io/github/titanium_knights/util/Carousel.java
+++ TeamCode/src/main/java/io/github/titanium_knights/util/Carousel.java
@@ -8,15 +8,15 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.jetbrains.annotations.NotNull;
 
 public class Carousel {
-    //    public CRServo motor;
+    public CRServo motor;
     public CRServo motor2;
 
     public Carousel(HardwareMap hmap){
-//        motor = hmap.get(CRServo.class,"carousel_l");
+        motor = hmap.get(CRServo.class,"carousel_l");
         motor2 = hmap.get(CRServo.class, "carousel_r");
     }
     public void setPower(double power) {
-//        motor.setPower(power);
+        motor.setPower(power);
         motor2.setPower(-power);
     }
     public void spin(boolean fast){
