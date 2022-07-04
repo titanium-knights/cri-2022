61b5f95f1600da5dcf80601df18290f1feeddda7
alisona le <alile24@bergen.org>
2022-07-04 00:19:18 -0400

    removed motor variable + commented out unused dump, idle, and isBusy

--- TeamCode/src/main/java/io/github/titanium_knights/util/Carriage.java
+++ TeamCode/src/main/java/io/github/titanium_knights/util/Carriage.java
@@ -13,14 +13,12 @@ import com.qualcomm.robotcore.hardware.Servo;
     public static int DUMP_POS = -192;
     public static int IDLE_POS = -5;
     public static double CARRIAGE_POWER = 0.5;
-    public final String MOTOR_NAME = "carriage";
-
 
     public static double RAMP_OPEN = 0;
     public static double RAMP_CLOSE = 0;
 
     public Carriage(HardwareMap hmap){
-        arm = hmap.get(DcMotor.class, MOTOR_NAME);
+        arm = hmap.get(DcMotor.class, "carriage");
         arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         ramp = hmap.get(Servo.class, "ramp");
         trapdoor = hmap.get(Servo.class, "trapdoor");
@@ -31,20 +29,20 @@ import com.qualcomm.robotcore.hardware.Servo;
     public int getPosition(){
         return arm.getCurrentPosition();
     }
+
     public void setPosition(int pos){
         arm.setTargetPosition(pos);
         arm.setPower(CARRIAGE_POWER);
     }
-    public void dump(){
-        this.setPosition(DUMP_POS);
-    }
-    public void idle(){
-        this.setPosition(IDLE_POS);
-    }
-    public boolean isBusy() {
-        return arm.isBusy();
-    }
-
+//    public void dump(){
+//        this.setPosition(DUMP_POS);
+//    }
+//    public void idle(){
+//        this.setPosition(IDLE_POS);
+//    }
+//    public boolean isBusy() {
+//        return arm.isBusy();
+//    }
 
     //new code
     public void setTrapdoorPos(double pos) {
