f3231eebf4b0cf40cf51a2f08bea70c22e39b744
alisona le <alile24@bergen.org>
2022-07-04 10:22:38 -0400

    added generic motor test from older tka code

---
+++ TeamCode/src/main/java/io/github/titanium_knights/testing/GenericMotorTest.java
@@ -0,0 +1,29 @@
+package io.github.titanium_knights.testing;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Config
+@TeleOp
+public class GenericMotorTest extends OpMode {
+    DcMotor motor;
+    public static String MOTOR_NAME = "carriage";
+    @Override
+    public void init() {
+        motor = hardwareMap.get(DcMotor.class, MOTOR_NAME);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);
+    }
+
+    @Override
+    public void loop() {
+        motor.setPower(gamepad1.left_stick_y/2);
+        telemetry.addData("Motor Encoder Val", motor.getCurrentPosition());
+        telemetry.update();
+    }
+}
