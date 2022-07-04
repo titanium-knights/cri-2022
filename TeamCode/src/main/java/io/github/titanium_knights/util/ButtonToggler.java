7bb011597dc27601c601c9c46f9f9955e34f1b0a
alisona le <alile24@bergen.org>
2022-07-04 00:36:21 -0400

    added button toggler from tkb

---
+++ TeamCode/src/main/java/io/github/titanium_knights/util/ButtonToggler.java
@@ -0,0 +1,40 @@
+package io.github.titanium_knights.util;
+
+public class ButtonToggler {
+
+    private boolean on;
+    private boolean mode;
+    private double duration;
+
+    public ButtonToggler() {
+        on = false;
+        mode = false;
+        duration = 0;
+    }
+
+    public boolean ifPress (boolean current) {
+        if (!on && current) {
+            mode = !mode;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean ifRelease (boolean current) {
+        if (on && !current) {
+            mode = !mode;
+            return true;
+        }
+
+        return false;
+    }
+
+    public void update (boolean current) {
+        on = current;
+    }
+
+    public boolean getMode () {
+        return mode;
+    }
+
+}
