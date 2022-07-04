d5e2c5d7529aaad552f7b76fbcbb4890796bc8c3
alisona le <alile24@bergen.org>
2022-07-04 00:17:24 -0400

    removed methods not used

--- TeamCode/src/main/java/io/github/titanium_knights/util/Claw.java
+++ TeamCode/src/main/java/io/github/titanium_knights/util/Claw.java
@@ -8,9 +8,8 @@ import org.jetbrains.annotations.NotNull;
 public class Claw {
     public Servo claw;
     public static double releasePos = 1.0;
-    public static double ballPos = 0.8;
     public static double grabPos = 0.75;
-    public static double armMultiplier = -0.5;
+//    public static double armMultiplier = -0.5;
 
     public Claw(HardwareMap hardwareMap) {
         this.claw = hardwareMap.servo.get("claw"); //slide open and close
@@ -20,21 +19,8 @@ public class Claw {
         claw.setPosition(grabPos);
     }
 
-    public void grabBall() {
-        claw.setPosition(ballPos);
-    }
-
     public void release() {
         claw.setPosition(releasePos);
-    }
-
-    public void incrementClawPosition(double amount) {
-        double position = claw.getPosition();
-
-        if (amount > 0 && position >= Math.max(releasePos, grabPos)) return;
-        if (amount < 0 && position <= Math.min(releasePos, grabPos)) return;
-
-        claw.setPosition(position + amount);
     }
 
 }
