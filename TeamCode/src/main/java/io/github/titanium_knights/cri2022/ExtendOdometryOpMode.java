3c0f25f2947072c9b7e1b150218112327f71b65e
alisona le <alile24@bergen.org>
2022-07-05 17:54:04 -0400

    fixed controls

---
+++ TeamCode/src/main/java/io/github/titanium_knights/cri2022/ExtendOdometryOpMode.java
@@ -0,0 +1,17 @@
+package io.github.titanium_knights.cri2022;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import io.github.titanium_knights.util.OdometryRetraction;
+
+@Autonomous(name = "Extend Odometry")
+public class ExtendOdometryOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        OdometryRetraction retraction = new OdometryRetraction(hardwareMap);
+        waitForStart();
+        retraction.extend();
+        sleep(1000);
+    }
+}
