9df482a3750ff8b0bbff422e414c7479d63dc3a5
Alice Zhang <anz0063@gmail.com>
2022-07-05 21:06:04 -0400

    afk auton start

---
+++ MeepMeepTesting/src/main/java/io/github/titanium_knights/cri2022/meepmeep/AFKPrototype.java
@@ -0,0 +1,7 @@
+package io.github.titanium_knights.cri2022.meepmeep;
+
+public class AFKPrototype {
+    public static void main(String[] args) {
+        CRIMeepMeep.create("/AFKPrototype.json").start();
+    }
+}
