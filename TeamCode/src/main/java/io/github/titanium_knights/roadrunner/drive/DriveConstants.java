499425d0bbeaa16a98e79c8534c9a5db621ba8cf
alisona le <alile24@bergen.org>
2022-07-05 21:44:59 -0400

    changed kv, ka, and kstatic at 13V (battery) --> looked well :-)

--- TeamCode/src/main/java/io/github/titanium_knights/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/io/github/titanium_knights/roadrunner/drive/DriveConstants.java
@@ -53,9 +53,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.016455 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0.08735;
-    public static double kStatic = 0.08735;
+    public static double kV = 0.01651 / rpmToVelocity(MAX_RPM);
+    public static double kA = 0.00326;
+    public static double kStatic = 0.0803;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
