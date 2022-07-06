b7a27f7dc2aa15550d5d7663f5b102becbf63dde
alisona le <alile24@bergen.org>
2022-07-06 10:26:44 -0400

    kv updated (changed to actual correct number)

--- TeamCode/src/main/java/io/github/titanium_knights/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/io/github/titanium_knights/roadrunner/drive/DriveConstants.java
@@ -53,7 +53,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.01631 / rpmToVelocity(MAX_RPM);
+    public static double kV = 0.01631;
     public static double kA = 0.00326;
     public static double kStatic = 0.0803;
 
