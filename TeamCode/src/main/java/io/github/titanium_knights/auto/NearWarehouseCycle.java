b9e2a6542b0bee627e5b2d24c0303fb99f6bf31c
Anthony Li <me@anli.dev>
2022-07-06 11:42:32 -0400

    Annotate autonomous op modes

--- TeamCode/src/main/java/io/github/titanium_knights/auto/NearWarehouseCycle.java
+++ TeamCode/src/main/java/io/github/titanium_knights/auto/NearWarehouseCycle.java
@@ -1,10 +1,12 @@
 package io.github.titanium_knights.auto;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import io.github.titanium_knights.roadrunner.drive.SampleMecanumDrive;
 import io.github.titanium_knights.util.JSONParsingUtils;
 import io.github.titanium_knights.util.TrajectorySession;
 
+@Autonomous(name = "Near Warehouse Cycle (Prototype)")
 public class NearWarehouseCycle extends LinearOpMode {
     public static String PATH = JSONParsingUtils.stringFromResource("/nearWarehousePrototype.json");
 
