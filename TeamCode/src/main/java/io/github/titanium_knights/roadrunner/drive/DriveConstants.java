0faddb22eb8378cc7bde9ee9980c73c39c16b8ab
alisona le <alile24@bergen.org>
2022-07-05 21:26:29 -0400

    changed kv, ka, and kstatic to old tka bot values (from mti)

--- TeamCode/src/main/java/io/github/titanium_knights/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/io/github/titanium_knights/roadrunner/drive/DriveConstants.java
@@ -53,9 +53,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.016455 / rpmToVelocity(MAX_RPM);
+    public static double kA = 0.08735;
+    public static double kStatic = 0.08735;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -64,7 +64,7 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 243.4785571975649;
+    public static double MAX_VEL = 51.89229847699897;
     public static double MAX_ACCEL = 52.48291908330528;
     public static double MAX_ANG_VEL = Math.toRadians(261.482587826087);
     public static double MAX_ANG_ACCEL = Math.toRadians(261.482587826087);
