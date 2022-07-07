84bae6bef72c67d49743b20a234b3cb1bb0a8131
Anthony Li <me@anli.dev>
2022-07-07 10:22:16 -0400

    Add NearWarehouseCycle callback names

--- TeamCode/src/main/java/io/github/titanium_knights/auto/NearWarehouseCycle.java
+++ TeamCode/src/main/java/io/github/titanium_knights/auto/NearWarehouseCycle.java
@@ -3,9 +3,7 @@ package io.github.titanium_knights.auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import io.github.titanium_knights.roadrunner.drive.SampleMecanumDrive;
-import io.github.titanium_knights.util.JSONParsingUtils;
-import io.github.titanium_knights.util.OdometryRetraction;
-import io.github.titanium_knights.util.TrajectorySession;
+import io.github.titanium_knights.util.*;
 
 @Autonomous(name = "Near Warehouse Cycle (Prototype)")
 public class NearWarehouseCycle extends LinearOpMode {
@@ -16,6 +14,43 @@ public class NearWarehouseCycle extends LinearOpMode {
         OdometryRetraction odometryRetraction = new OdometryRetraction(hardwareMap);
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         TrajectorySession session = TrajectorySession.buildFromJSON(drive, PATH);
+        Slides slides = new Slides(hardwareMap);
+        Carriage carriage = new Carriage(hardwareMap);
+
+        session.registerCallback("startIntake", () -> {
+
+        });
+
+        session.registerCallback("stopIntake", () -> {
+
+        });
+
+        session.registerCallback("extendSlides", () -> {
+            slides.runToPosition(Slides.MAX_POSITION);
+        });
+
+        session.registerCallback("raiseCarriage", () -> {
+            carriage.setArmPosition(Carriage.ARM_MAX);
+        });
+
+        session.registerCallback("openTrapdoor", () -> {
+            carriage.setTrapdoorPos(Carriage.TRAPDOOR_DUMP_POS);
+        });
+
+        session.registerCallback("lowerCarriage", () -> {
+            carriage.setTrapdoorPos(Carriage.TRAPDOOR_IDLE_POS);
+            carriage.setArmPosition(Carriage.ARM_SAFE_POSITION);
+        });
+
+        session.registerCallback("retractSlides", () -> {
+            slides.runToPosition(Slides.MIN_POSITION);
+        });
+
+        session.registerCallback("openRamp", () -> {
+            carriage.setRampPos(Carriage.RAMP_OPEN);
+            carriage.setArmPosition(0);
+        });
+
         odometryRetraction.extend();
         waitForStart();
         session.run(this);
