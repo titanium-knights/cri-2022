aaa06fdd68f360c102d5aa93021d5c3698765b37
Alice Zhang <anz0063@gmail.com>
2022-07-07 21:58:54 -0400

    VERY SCUFFED FAR WAREHOUSE AUTON IF IT WORKS I THANK THE LORD

---
+++ MeepMeepTesting/src/main/java/io/github/titanium_knights/cri2022/meepmeep/FarWarehouseBlue.java
@@ -0,0 +1,8 @@
+package io.github.titanium_knights.cri2022.meepmeep;
+
+public class FarWarehouseBlue {
+    public static void main(String[] args) {
+        CRIMeepMeep.create("/FarWarehouseBlue.json").start();
+    }
+}
+
---
+++ MeepMeepTesting/src/main/java/io/github/titanium_knights/cri2022/meepmeep/FarWarehouseRed.java
@@ -0,0 +1,7 @@
+package io.github.titanium_knights.cri2022.meepmeep;
+
+public class FarWarehouseRed {
+    public static void main(String[] args) {
+        CRIMeepMeep.create("/FarWarehouseRed.json").start();
+    }
+}
