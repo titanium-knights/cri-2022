aa283dc0ec51e680c8aa8426a8fcbba9bb0b65da
Anthony Li <me@anli.dev>
2022-07-09 14:26:27 -0500

    Fck.
    Co-Authored-By: Alice Zhang <anz0063@gmail.com>

--- TeamCode/src/main/java/io/github/titanium_knights/auto/NearWarehouseDuckRed.java
+++ TeamCode/src/main/java/io/github/titanium_knights/auto/NearWarehouseDuckRed.java
@@ -2,7 +2,7 @@ package io.github.titanium_knights.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "Near Warehouse (Red)")
+@Autonomous(name = "Near Warehouse Duck (Red)")
 public class NearWarehouseDuckRed extends NearWarehouseDuck {
     @Override
     boolean isRed() {
